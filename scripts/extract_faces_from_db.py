4f766dbaca52c9f9d51eedda73aae39a8448bf2f
Rodolfo Ferro <rodolfoferroperez@gmail.com>
2017-12-23 03:36:56 -0600

    Initial commit.

---
+++ scripts/extract_faces_from_db.py
@@ -0,0 +1,71 @@
+from tqdm import tqdm
+import glob
+import cv2
+
+
+# Path to Haar Cascades:
+cvpath = "../haarcascades/"
+face_det_01 = cv2.CascadeClassifier(
+    cvpath + "haarcascade_frontalface_default.xml")
+face_det_02 = cv2.CascadeClassifier(
+    cvpath + "haarcascade_frontalface_alt2.xml")
+face_det_03 = cv2.CascadeClassifier(
+    cvpath + "haarcascade_frontalface_alt.xml")
+face_det_04 = cv2.CascadeClassifier(
+    cvpath + "haarcascade_frontalface_alt_tree.xml")
+
+label = "Rod"
+
+
+def extract_faces():
+    files = sorted(glob.glob("../.db/original/*"))
+
+    filenumber = 1
+    for f in tqdm(files):
+        img = cv2.imread(f)
+
+        # Convert to grayscale:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Detect face using 4 different classifiers:
+        face_01 = face_det_01.detectMultiScale(
+            gray, scaleFactor=1.1, minNeighbors=5,
+            minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
+        face_02 = face_det_02.detectMultiScale(
+            gray, scaleFactor=1.1, minNeighbors=5,
+            minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
+        face_03 = face_det_03.detectMultiScale(
+            gray, scaleFactor=1.1, minNeighbors=5,
+            minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
+        face_04 = face_det_04.detectMultiScale(
+            gray, scaleFactor=1.1, minNeighbors=5,
+            minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
+
+        # Detect only one face:
+        if len(face_01) == 1:
+            facefeatures = face_01
+        elif len(face_02) == 1:
+            facefeatures = face_02
+        elif len(face_03) == 1:
+            facefeatures = face_03
+        elif len(face_04) == 1:
+            facefeatures = face_04
+        else:
+            facefeatures = ""
+
+        for (x, y, w, h) in facefeatures:
+            # pri3t("Face found in file: {}".format(f))
+            # Crop face:
+            new = gray[y:y + h, x:x + w]
+
+            try:
+                # Resize & save face:
+                out = cv2.resize(new, (350, 350))
+                cv2.imwrite("../.db/cleaned/{}.jpg".format(filenumber), out)
+            except:
+                pass
+        filenumber += 1
+
+
+if __name__ == "__main__":
+    extract_faces()
---
+++ scripts/lbph_recognizer.py
@@ -0,0 +1,84 @@
+from tqdm import tqdm
+import numpy as np
+import glob
+import cv2
+
+
+# Path to Haar Cascades:
+cvpath = "../haarcascades/"
+casc = cv2.CascadeClassifier(cvpath + "haarcascade_frontalface_default.xml")
+
+# Subjects:
+subjects = ["Rodolfo Ferro"]
+
+# Fisher Face classifier:
+lbhpface = cv2.face.createLBPHFaceRecognizer()
+
+
+def recognize_person():
+    # Create datasets:
+    train_lbls, train_data = [], []
+    train = sorted(glob.glob("../.db/cleaned/*"))
+    for item in train:
+        image = cv2.imread(item)
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        train_data.append(gray)
+        train_lbls.append(0)
+
+    # Train model:
+    print("\n\n{:=<40}".format(""))
+    print("Train LBHP Face Classifier.")
+    print("Size of training set is {} images.".format(len(train_lbls)))
+    lbhpface.train(train_data, np.array(train_lbls))
+
+    # Load video capture:
+    cap = cv2.VideoCapture(0)
+
+    while(True):
+        # Capture frame-by-frame:
+        ret, img = cap.read()
+
+        # Convert to grayscale:
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        # Detect face using 4 different classifiers:
+        faces = casc.detectMultiScale(
+                gray, scaleFactor=1.3, minNeighbors=10,
+                minSize=(5, 5), flags=cv2.CASCADE_SCALE_IMAGE)
+
+        for i, (x, y, w, h) in enumerate(faces):
+            # Crop face:
+            out = gray[y:y + h, x:x + w]
+
+            # Resize & save face:
+            out = cv2.resize(out, (350, 350))
+
+            # Predict face:
+            pred = lbhpface.predict(out)
+            cv2.rectangle(img, (x, y), (x + w, y + h), (180, 255, 10), 2)
+
+            if pred[1] < 45:
+                # cv2.putText(img, 'Face {}'.format(subjects[pred[0]]),
+                cv2.putText(img, '{}'.format(subject[0]),
+                            (x + w//6, y - 15), cv2.FONT_HERSHEY_SIMPLEX,
+                            0.003*w, (255, 180, 10), 2, cv2.LINE_AA)
+            else:
+                cv2.putText(img, 'Not recognized',
+                            (x + w//6, y - 15), cv2.FONT_HERSHEY_SIMPLEX,
+                            0.003*w, (255, 180, 10), 2, cv2.LINE_AA)
+            # print("Face: {}. Pred: {}".format(i, pred))
+
+        # Display the resulting frame
+        new = cv2.resize(img, (640, 360))
+        cv2.imshow('Out', new)
+        key = cv2.waitKey(10)
+        if key == 27:
+            break
+
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    recognize_person()
